2fa4414b880aaf4eed029ee13f8b56d9b3e4be56
Adam Rudd <adam.rudd@uqconnect.edu.au>
2012-02-07 11:12:11 +1000

    Use path.join
    ...Rather than making your own stupid solution to the problem!

--- mqttfs.js
+++ mqttfs.js
@@ -9,6 +9,7 @@ var debug_on = process.env['debug'] || process.env['DEBUG'] || false;
 
 var argv = require('optimist')
 	.usage('Explore an MQTT hierarchy as a file system\nUsage: $0 -p <broker port> -h <broker hostname> -b <base directory>')
+	.describe('help', 'print this message')
 	.options('p', {
 		alias: 'port',
 		describe: 'Broker port',
@@ -27,15 +28,20 @@ var argv = require('optimist')
 	.options('b', {
 		alias: 'path',
 		describe: 'filesystem base path',
-		default: __dirname + '/mqttfs'
+		default: './mqttfs'
 	})
 	.argv
 
 
-var base = argv.b + '/',
+var base = argv.b,
 	port = argv.p,
 	host = argv.h,
 	topics = argv.t.split(',');
+
+if (argv.help) {
+	require('optimist').showHelp();
+	process.exit(1);
+}
 
 function debug() {
 	if (debug_on) console.log.apply(this, arguments);
@@ -58,13 +64,13 @@ function createBaseDir(base, cb) {
 }
 
 function mkdir(dir, cb) {
-	var dir = (dir[0] === '/' ? dir.substr(1) : dir),
-		cb = cb || nop;
+	var cb = cb || nop,
+		joined = path.join(base, dir);
 
-	path.exists(base + dir, function(exists) {
-		if (exists) return cb(null, base + dir + ' already exists');
+	path.exists(joined, function(exists) {
+		if (exists) return cb(null, joined + ' already exists');
 
-		var mkdir = spawn('mkdir', ['-p', base + dir]);
+		var mkdir = spawn('mkdir', ['-p', joined]);
 
 		mkdir.stderr.setEncoding('utf8');
 		mkdir.stderr.on('data', function(data) {
@@ -73,7 +79,7 @@ function mkdir(dir, cb) {
 		});
 
 		mkdir.on('exit', function(code, signal) {
-			if (code === 0) return cb(null, base + dir + ' created');
+			if (code === 0) return cb(null, joined + ' created');
 
 			return cb(mkdir.errors.join('\nCode: ' + code));
 		});
@@ -82,18 +88,20 @@ function mkdir(dir, cb) {
 }
 
 function onpublish(topic, payload) {
-	var psplit = topic.split('/'),
-		dir = psplit.slice(0, psplit.length - 1).join('/');
-		file = psplit[psplit.length - 1];
+	var dir = path.dirname(topic),
+		file = path.join(base, topic);
 
 	mkdir(dir, function(err, result) {
-		var stream = fs.createWriteStream(base + topic, {
+		if (err) return debug(err);
+		debug('Opening %s', file);
+		var stream = fs.createWriteStream(file, {
 			flags: 'a',
 			encoding: 'utf8',
 			mode: 0777
 		});
 
 		stream.on('open', function(fd) {
+			debug('Writing'); 
 			stream.write(util.format('[%s] Topic: %s, Payload: %s\n', ''+new Date(), topic, payload));
 		});
 	});
@@ -106,9 +114,11 @@ mqtt.createClient(port, host, function(client) {
 
 	client.on('connack', function(packet) {
 		debug('Connected');
-		client.subscribe({
-			subscriptions: topics.map(function(x) { return {topic: x}; })
-		});
+		createBaseDir(base, debug);
+		debug('Subscribing to %j', topics);
+		debug('Subscribe: %s', '' + client.subscribe({
+			subscriptions: topics
+		}));
 
 		setInterval(function() {
 			client.pingreq();
